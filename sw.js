92f186f4cc27e965a3f07d7a007d14b124c4ade0
Nathanobi <nathan.bey@viacesi.fr>
2026-03-11 20:46:50 +0100

    Summary 1.49
    Essaie pour voir l'erreur IPhone

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Stratégie : Cache First pour les assets statiques,
 //             Network First pour les requêtes réseau (API PK, imgbb)
 
-const VERSION   = 'plural-space-v48';
+const VERSION   = 'plural-space-v49';
 const CACHE_STATIC = `${VERSION}-static`;
 
 // Assets à précacher — toute l'app shell
